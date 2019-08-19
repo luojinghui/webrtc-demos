5a210e971e453f299c6b289440d470b61eafba31
luojinghui <luojinghui@xylink.com>
2019-08-19 11:25:22 +0800

    feat: dynamic adjustment resolution value

--- src/view/webrtc/index.jsx
+++ src/view/webrtc/index.jsx
@@ -13,7 +13,8 @@ export default class Webrtc extends Component {
 			videoInList: [],
 			selectAudioInputValue: '',
 			selectAudioOutputValue: '',
-			selectvideoInValue: ''
+			selectvideoInValue: '',
+			showOperate: false
 		};
 
 		this.handleSuccess = this.handleSuccess.bind(this);
@@ -22,9 +23,13 @@ export default class Webrtc extends Component {
 		this.onChangeAudioInput = this.onChangeAudioInput.bind(this);
 		this.onChangeVideoInput = this.onChangeVideoInput.bind(this);
 		this.onChangeAudioOutput = this.onChangeAudioOutput.bind(this);
+		this.onHandleChangeResolution = this.onHandleChangeResolution.bind(this);
+    this.onHandleStop = this.onHandleStop.bind(this);
+    
+    this.stream = null;
 	}
 
-	get constraints() {
+	constraints(videoObj = {}) {
 		const { selectAudioInputValue, selectvideoInValue } = this.state;
 
 		return {
@@ -34,14 +39,15 @@ export default class Webrtc extends Component {
 			video: {
 				deviceId: selectvideoInValue ? { exact: selectvideoInValue } : undefined,
 				aspectRatio: 1.778,
-				resizeMode: 'crop-and-scale'
+        resizeMode: 'crop-and-scale',
+        ...videoObj
 			}
 		};
-	}
+  }
 
 	handleError(error) {
 		if (error.name === 'ConstraintNotSatisfiedError') {
-			let v = this.constraints.video;
+			let v = this.constraints().video;
 			console.log(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
 		} else if (error.name === 'PermissionDeniedError') {
 			console.log(
@@ -54,16 +60,34 @@ export default class Webrtc extends Component {
 	}
 
 	async onHandleOpenCamera(e) {
-		try {
-			// MediaDevices 是一个单例对象。通常，您只需直接使用此对象的成员，例如通过调用
-			const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
-			this.handleSuccess(stream);
+    if (this.stream) {
+      this.stream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
 
-			console.log('windowstram: ', window.stream);
+		try {
+      // MediaDevices 是一个单例对象。通常，您只需直接使用此对象的成员，例如通过调用
+      console.log("this.constranits: ", this.constraints());
+			const stream = await navigator.mediaDevices.getUserMedia(this.constraints());
+			this.handleSuccess(stream);
 		} catch (e) {
 			this.handleError(e);
 		}
-	}
+  }
+  
+  async getMedia(constraints) {
+    if (this.stream) {
+      this.stream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+
+    console.log("constraints: ", constraints);
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    this.handleSuccess(stream);
+  }
 
 	handleSuccess(stream) {
 		const videoTracks = stream.getVideoTracks();
@@ -72,10 +96,13 @@ export default class Webrtc extends Component {
 		console.log(`Using video device: ${videoTracks[0].kind}`);
 		console.log(`Using video device: ${audioTracks}`);
 		console.log(`stream: ${stream}`);
-		window.stream = stream; // make variable available to browser console
+    window.stream = stream; // make variable available to browser console
+    this.stream = stream;
 
 		this.setState({
-			videos: [stream]
+			videos: [stream],
+			localMedia: stream,
+			showOperate: true
 		});
 	}
 
@@ -118,7 +145,7 @@ export default class Webrtc extends Component {
 				audioOutputList,
 				videoInList,
 				selectAudioInputValue: audioInputList[0].value,
-				selectAudioOutputValue: audioOutputList[0] ? audioOutputList[0].value : "",
+				selectAudioOutputValue: audioOutputList[0] ? audioOutputList[0].value : '',
 				selectvideoInValue: videoInList[0].value
 			});
 		} catch (error) {
@@ -146,8 +173,14 @@ export default class Webrtc extends Component {
 					selectAudioInputValue: e.target.value
 				},
 				() => {
-					this.onHandleOpenCamera({});
-				}
+					// this.onHandleOpenCamera({});
+          if(this.state.localMedia) {
+            console.log("audio inp0ut...");
+            const track = this.state.localMedia.getVideoTracks()[0];
+
+            track.applyConstraints(this.constraints())
+          }
+        }
 			);
 		}
 	}
@@ -159,15 +192,21 @@ export default class Webrtc extends Component {
 					selectvideoInValue: e.target.value
 				},
 				() => {
-					this.onHandleOpenCamera({});
+					// this.onHandleOpenCamera({});
+          if(this.state.localMedia) {
+            console.log(123);
+            const track = this.state.localMedia.getVideoTracks()[0];
+
+            track.applyConstraints(this.constraints())
+          }
 				}
 			);
 		}
 	}
 
 	async attachSinkId() {
-    const element = this.video && this.video.getVideoRef();
-    const { selectAudioOutputValue: sinkId } = this.state;
+		const element = this.video && this.video.getVideoRef();
+		const { selectAudioOutputValue: sinkId } = this.state;
 
 		if (typeof element.sinkId !== 'undefined') {
 			try {
@@ -188,9 +227,9 @@ export default class Webrtc extends Component {
 		} else {
 			console.warn('Browser does not support output device selection.');
 		}
-  }
-  
-  onChangeAudioOutput(e) {
+	}
+
+	onChangeAudioOutput(e) {
 		if (this.state.selectAudioOutputValue !== e.target.value) {
 			this.setState(
 				{
@@ -200,11 +239,60 @@ export default class Webrtc extends Component {
 					this.attachSinkId();
 				}
 			);
-		}    
+		}
+	}
+
+	onHandleChangeResolution(type) {
+    console.log('type: ', type);
+    const resolutionMap = {
+      "QVQA": {width: {exact: 320}, height: {exact: 240}},
+      "VGA": {width: {exact: 640}, height: {exact: 480}},
+      "HD": {width: {exact: 1280}, height: {exact: 720}},
+      "FULLHD": {width: {exact: 1920}, height: {exact: 1080}},
+      "4K": {width: {exact: 4096}, height: {exact: 2160}}
+    };
+
+    // const newConstraints = this.constraints(resolutionMap[type]);
+    const newConstraints = resolutionMap[type];
+
+    // this.getMedia(newCons);
+    try {
+      if(this.stream) {
+        console.log("newConstraints2:", newConstraints);
+        const track = this.stream.getVideoTracks()[0];
+  
+        track.applyConstraints(newConstraints).then(() => {
+          console.log("success");
+        }).catch((err) => {
+          console.log("set resolution err: ", err);    
+        });
+      }
+    } catch(err) {
+      console.log("set resolution err: ", err);
+    }
+
+  }
+  
+  onHandleStop() {
+    if(this.state.localMedia) {
+      const track = this.state.localMedia.getVideoTracks()[0];
+      const track2 = this.state.localMedia.getAudioTracks()[0];
+
+      track.stop();
+      track2.stop();
+    }
   }
 
 	render() {
-		const { audioInputList, audioOutputList, videoInList, selectAudioInputValue, selectvideoInValue, selectAudioOutputValue } = this.state;
+		const {
+			audioInputList,
+			audioOutputList,
+			videoInList,
+			selectAudioInputValue,
+			selectvideoInValue,
+			selectAudioOutputValue,
+			showOperate
+		} = this.state;
 
 		return (
 			<div className="container">
@@ -217,43 +305,81 @@ export default class Webrtc extends Component {
 				</div>
 				<div className="btns">
 					<button onClick={this.onHandleOpenCamera}>Open camera</button>
+          <button onClick={this.onHandleStop}>Stop</button>
 					<button onClick={this.onSnapshot}>take snapshot</button>
+					<button
+						onClick={() => {
+							this.onHandleChangeResolution('QVQA');
+						}}
+					>
+						QVGA
+					</button>
+					<button
+						onClick={() => {
+							this.onHandleChangeResolution('VGA');
+						}}
+					>
+						VGA
+					</button>
+					<button
+						onClick={() => {
+							this.onHandleChangeResolution('HD');
+						}}
+					>
+						HD
+					</button>
+					<button
+						onClick={() => {
+							this.onHandleChangeResolution('FULLHD');
+						}}
+					>
+						Full HD
+					</button>
+					<button
+						onClick={() => {
+							this.onHandleChangeResolution('4K');
+						}}
+					>
+						4K
+					</button>
 				</div>
-				<div className="select">
-					<div className="audio-input">
-						<label>Audio input source:</label>
-						<select onChange={this.onChangeAudioInput} value={selectAudioInputValue}>
-							{audioInputList.map(item => (
-								<option key={item.value} value={item.value}>
-									{item.text}
-								</option>
-							))}
-						</select>
-					</div>
+				{showOperate && (
+					<div className="select">
+						<div className="audio-input">
+							<label>Audio input source:</label>
+							<select onChange={this.onChangeAudioInput} value={selectAudioInputValue}>
+								{audioInputList.map(item => (
+									<option key={item.value} value={item.value}>
+										{item.text}
+									</option>
+								))}
+							</select>
+						</div>
 
-					<div className="audio-output">
-						<label>Audio output source:</label>
-						<select onChange={this.onChangeAudioOutput} value={ selectAudioOutputValue }>
-							{audioOutputList.map(item => (
-								<option key={item.value} value={item.value}>
-									{item.text}
-								</option>
-							))}
-						</select>
-					</div>
+						<div className="audio-output">
+							<label>Audio output source:</label>
+							<select onChange={this.onChangeAudioOutput} value={selectAudioOutputValue}>
+								{audioOutputList.map(item => (
+									<option key={item.value} value={item.value}>
+										{item.text}
+									</option>
+								))}
+							</select>
+						</div>
 
-					<div className="video-source">
-						<label>Video source:</label>
-						<select onChange={this.onChangeVideoInput} value={selectvideoInValue}>
-							>
-							{videoInList.map(item => (
-								<option key={item.value} value={item.value}>
-									{item.text}
-								</option>
-							))}
-						</select>
+						<div className="video-source">
+							<label>Video source:</label>
+							<select onChange={this.onChangeVideoInput} value={selectvideoInValue}>
+								>
+								{videoInList.map(item => (
+									<option key={item.value} value={item.value}>
+										{item.text}
+									</option>
+								))}
+							</select>
+						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		);
 	}
